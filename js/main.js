7190de2a889a195ce5d0b7bbd156a62ed9403faf
dimput <dimasputra@mail.ugm.ac.id>
2019-03-14 09:29:23 +0700

    PWA

--- js/main.js
+++ js/main.js
@@ -4,75 +4,108 @@ $(document).ready(function(){
 
     //var _url = "http://localhost:8787/json.php";
     //Store Data
+
+    // menampung data yang didapat dari API
     var result = '';
 
-    //Store Gender for Option
+    // menampung gender sbg option
     var gender_opt = '';
-    gender_opt += '<option value="Semua">Semua</option>';
 
-    //Store All gener from API
+    // menampung semua gender dari API
     var gender = [];
 
-    $.get (_url,function(data){
-        $.each(data,function(key,items){
-            //Store Gender
+    // $.get(_url,function (data) {
+    function renderPage(data) {
+        $.each(data, function (key, items) {
+            // untuk menampung gender sementara pd loop
             _gend = items.gender;
-            result +=   '<div class="col-3 card">' +
-                            '<p><b>' + items.name + '</b></p>' +
-                            '<p>' + _gend + '</p>' +
-                        '</div>';
-            if ($.inArray(_gend, gender) === -1){
-                //if gender not Found
-                //then input gender opt
+
+            // untuk memasukkan data ke result dari API
+            result += 
+                '<div class="col-3 card">' +
+                    '<p><b>' + items.name + '</b></p>' +
+                    '<p>' + _gend + '</p>' +
+                '</div>';
+
+            // jika gender tidak ada didalam array gender,
+            // maka masukkan gender opt
+            if ($.inArray(_gend, gender) === -1) {
+                // data gender di push untuk
+                // pengecekan itrasi berikutnya
                 gender.push(_gend);
-                gender_opt += '<option value="' + _gend + '">' + _gend +'</option>';
+                // set gender_opt dgn <option>
+                gender_opt += '<option value="' + _gend +
+                    '">' + _gend + '</option>'
             }
         });
 
-    // use Selector by Id mhs-list
-    // Replace inner HTML
-    $('#mhs-list').html(result);
+        // menggunakan selector ID mhs-list,
+        // kemudian replace html di dalam komponen yang
+        // ada di id mhs-list menjadi result
+        $('#mhs-list').html(result);
 
-    /**
-     * 
-     */
-    $('#gender-select').html(gender_opt);
+        // menggunakan selector ID gender-select,
+        // kemudian replace html di dalam komponen yang
+        // ada di id gender-select menjadi gender_opt
+        $('#gender-select').html('<option value="semua">semua</option>' + gender_opt);
+        // });
+    }
+
+    var networkDataReceive = false;
+    /* cek di Cache, apakah sudah ada belum, ngambil data dari service online */
+    var networkUpdate = fetch(_url).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        networkDataReceive = true;
+        renderPage(data)
+    });
     
-    $('#gender-select').on('change',function(){
-        updateData($(this).val());
+    /*fetch data dari cache*/
+    caches.match(_url).then(function (response) {
+        if (!response) throw Error("no data on cache");
+        return response.json();
+    }).then(function (data) {
+        if (!networkDataReceive){
+            renderPage(data);
+            console.log('render data from cache');
+        }
+    }).catch(function () {
+        return networkUpdate;
     })
 
+
+    // filter untuk option gender
+    $('#gender-select').on('change', function () {
+        updateList($(this).val());
     });
 
+    function updateList(opt) {
+        var _url2 = _url;
 
-});
+        if (opt !== 'semua'){
+            _url2 = _url + '?gender='+opt;
+        }
+        // menampung data yang didapat dari API
+        var result = '';
 
-function updateData(_gend){
-    newResult = ''
+        $.get(_url2,function (data) {
+            $.each(data, function (key, items) {
+                // untuk menampung gender sementara pd loop
+                _gend = items.gender;
 
-    var _newUrl = "http://my-json-server.typicode.com/dimput/latihan-pwa-public/mahasiswa";
-
-    if(_gend!='Semua'){
-        _newUrl = _newUrl + "?gender=" + _gend;
-    }
-    console.log(_newUrl);
-    $.get (_newUrl,function(data){
-        $.each(data,function(key,items){
-            //Store Gender
-            _gend = items.gender;
-            newResult +=   '<div class="col-3 card wow fadeIn">' +
+                // untuk memasukkan data ke result dari API
+                result += 
+                        '<div class="col-3 card">' +
                             '<p><b>' + items.name + '</b></p>' +
                             '<p>' + _gend + '</p>' +
                         '</div>';
+            });
+
+            // update list
+            $('#mhs-list').html(result);
         });
-
-    // use Selector by Id mhs-list
-    // Replace inner HTML
-    $('#mhs-list').html(newResult);
-    })
-}
-
-
+    }
+});
 
 //Service Worker
 if ('serviceWorker' in navigator) {
--- serviceworker.js
+++ serviceworker.js
@@ -4,44 +4,79 @@ var urlsToCache = [
   '/css/main.css',
   '/js/main.js',
   '/images/logo-monster.png',
-  '/js/jquery.min.js'
+  '/js/jquery.min.js',
+  '/wow.css',
+  '/wow.js',
+  '/css/bootstrap.css'
 ];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache',cache);
-        return cache.addAll(urlsToCache);
+      caches.open(CACHE_NAME).then(
+          function (cache) {
+              console.log('service worker do install..', cache);
+              return cache.addAll(urlsToCache);
+          },
+          // function (err) {
+          //     console.log('err : ' , err);
+          // }
+      )
+  )
+});
+
+/* aktivasi cache */
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+      caches.keys().then(function (cacheNames) {
+          return Promise.all(
+              // delete cache jika ada versi  lebih baru
+              cacheName.filter(function (cacheName) {
+                  return cacheName !== CACHE_NAME;
+              }).map(function (cacheName) {
+                  return caches.delete(cacheName);
+              })
+          );
       })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        }
-      )
-    );
-  });
+/* fetch cache */
+self.addEventListener('fetch', function (event) {
+  var request = event.request;
+  var url = new URL(request.url);
 
-self.addEventListener('activate', function(event) {  
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.filter(function(cacheName){
-              return cacheName != CACHE_NAME
-          }).map(function(cacheName){
-              return caches.delete(cacheName)
+  // memisahkan cache  file dengan cache data API
+  if (url.origin === location.origin){
+      event.respondWith(
+          caches.match(request).then(function (response) {
+              return response || fetch(request);
           })
-        );
-      })
-    );
-  });
+      )
+  } else{
+      event.respondWith(
+          caches.open('list-mahasiswa-cache-v1')
+              .then(function (cache) {
+              return fetch(request).then(function (liveRequest) {
+                  cache.put(request, liveRequest.clone());
+                  return liveRequest;
+              }).catch(function () {
+                  return caches.match(request)
+                      .then(function (response) {
+                      if (response) return response;
+                      return caches.match('/fallback.json');
+                  })
+              })
+          })
+      )
+  }
+
+  // event.respondWith(
+  //     caches.match(event.request)
+  //         .then(function (response) {
+  //         console.log(response);
+  //         if (response){
+  //             return response;
+  //         }
+  //         return fetch(event.request);
+  //     })
+  // )
+})
