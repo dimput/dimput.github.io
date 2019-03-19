a8a38067a0b1e2fe88d3f801c70db4d1bccb3fe2
dimput <dimasputra@mail.ugm.ac.id>
2019-03-19 13:21:51 +0700

    background Sync

--- js/main.js
+++ js/main.js
@@ -142,19 +142,64 @@ $(document).ready(function(){
         console.log('www');
         displayNotification();
     })
+    
+    function isOnline(){
+        $status = $('#connection-status');
+        if(navigator.online){
+            status.html = '<p>anda Online</p>';
+        }
+        else {
+            status.html = '<p>anda Offline</p>';
+        }
+        window.addEventListene('online',isOnline);
+        window.addEventListener('offline',isOnline);
+        isOnline();
+    }
+
 });
+
 
 
 
 //Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
+      navigator.serviceWorker.register('/serviceworker.js')
+        .then(function(reg){
+            return navigator.serviceWorker.ready;
+        }).then(function(reg){
+            document.getElementById('req-sync')
+            .addEventListener('click',function(){
+                reg.sync.register('image-fetch').then(()=>{
+                    console.log('syn-register');
+                }).catch(function(err){
+                    console.log('fetch image gagal, Error :',err);
+                });
+            });
+        })
         // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        // console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
       });
+    }
+
+self.addEventListener('sync',function(event){
+    console.log('firing sync');
+    if(event.tag === 'image-fetch'){
+        console.log('sync event fired');
+        event.waitUntul(fetchImage());
+    }
+});
+
+function fetchImage(){
+    console.log('Firing : doSomeStuff()');
+    fetch('/images/logo-monster.png').then(function(response){
+        return response;
+    }).then(function(text){
+        console.log('Request Succes : ',text);
+    }).catch(function(err){
+        console.log('Request Failed : ', err);
     });
-  }
+}
